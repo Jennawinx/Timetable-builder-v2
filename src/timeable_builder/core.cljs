4f4170d31b1f3b65fa6306191c128393a9cf7a79
Jenny Quach <jennyquach.99@hotmail.com>
2022-01-15 11:19:12 -0500

    Split render functions more modularly

--- src/timeable_builder/core.cljs
+++ src/timeable_builder/core.cljs
@@ -43,6 +43,33 @@
     (.click link)
     (.removeChild (.-body js/document) link)))
 
+(defn filename-safe-datetime []
+  (-> (.toLocaleString (js/Date.))
+      (string/replace #"[\s]" "_")
+      (string/replace #"[,]" "")
+      (string/replace #"[/:]" "-")))
+
+(defn tag-suggestions 
+  "Walks through timeblocks data for used tags
+   Returns [{:keys [value]}]"
+  [time-blocks]
+  (->> time-blocks
+       (reduce
+        (fn [result [day time-blocks]]
+          (reduce
+           (fn [result [time time-block]]
+             (if (nil? (:tags time-block))
+               result
+               (reduce (fn [result tag]
+                         (conj result {:value (:value tag)}))
+                       result
+                       (:tags time-block))))
+           result
+           time-blocks))
+        [])
+       (distinct)
+       (sort-by :value)))
+
 (defn action-buttons [state]
   [:<>
    [:div "Actions: "]
@@ -52,22 +79,15 @@
                      :on-click #(local-save! (get-in @state [:timetable :time-blocks]))}
       "Local Save"]]
     [col/col {:lg 24 :sm 8}
-     [button/button {:block true
+     [button/button {:block    true
                      :on-click #(download-as-edn!
                                  (get-in @state [:timetable :time-blocks])
-                                 (str "timetable_"
-                                      (-> (.toLocaleString (js/Date.))
-                                          (string/replace #"[\s]" "_")
-                                          (string/replace #"[,]" "")
-                                          (string/replace #"[/:]" "-"))
-                                      ".edn"))}
+                                 (str "timetable_" (filename-safe-datetime) ".edn"))}
       "Download Data"]]
     [col/col {:lg 24 :sm 8}
      [upload/upload {:name      :file
                      :file-list []
                      :class     "upload-btn"
-                    ;;  :style     {:class "full-width"}
-                    ;;  :list-type :picture-card
                      :before-upload
                      (fn [file]
                        (-> (.text file)
@@ -79,15 +99,55 @@
       [button/button {:block true}
        "Load Data"]]]]])
 
+(defn tag-selector [time-blocks disabled? get-property set-property!]
+  [row/row
+   [:div "Tags: "]
+   [select/select
+    {:mode           :tags
+     :style          {:width :100%}
+     :show-arrow     true
+     :disabled       disabled?
+     :on-select      (fn [obj]
+                       (when-not (string/blank? (.-label obj))
+                         (let [s             (.-label obj)
+                               [label color] (string/split s ":")]
+                           (set-property! :tags
+                                          (concat (get-property :tags)
+                                                  [{:label label
+                                                    :value s
+                                                    :color color}])))))
+     :label-in-value true
+     :value          (or (get-property :tags) [])
+     :options        (tag-suggestions time-blocks)
+     :tag-render     (fn [props]
+                       (let [{:strs [value closable onClose]} (js->clj props)
+                             [label color]                    (string/split value ":")]
+                         (r/as-element
+                          [tag/tag
+                           {:style         {:background-color color}
+                            :class         "tag"
+                            :on-mouse-down (fn [e]
+                                             (.preventDefault e)
+                                             (.stopPropagation e))
+                            :closable      closable
+                            :on-close      (fn [& args]
+                                             (apply onClose args)
+                                             (set-property! :tags
+                                                            (->> (get-property :tags)
+                                                                 (filter (comp (partial not= label) :label))
+                                                                 (vec))))}
+                           label])))}]])
+
 (defn toolbar [state]
   (let [selection     (get-in @state [:timetable :selected])
+        timeblocks    (get-in @state [:timetable :time-blocks])
         set-property! (fn [field value]
                         (swap! state assoc-in (concat [:timetable :time-blocks] selection [field])
                                value))
         get-property  (fn [field]
                         (get-in @state (concat [:timetable :time-blocks] selection [field])))]
-    [row/row {:style  {:padding :1em}
-              :gutter 12}
+    [row/row {:gutter 12 
+              :style  {:padding :1em}}
      [col/col {:span 8}
       [row/row
        [col/col {:flex :auto}
@@ -111,56 +171,7 @@
           :value     (or (get-property :font-color) "black")
           :disabled  (nil? selection)
           :on-change #(set-property! :font-color (element-value %))}]]]
-      [row/row
-       [:div "Tags: "]
-       [select/select
-        {:mode           :tags
-         :style          {:width :100%}
-         :show-arrow     true
-         :disabled       (nil? selection)
-         :on-select      (fn [obj]
-                           (when-not (string/blank? (.-label obj))
-                             (let [s             (.-label obj)
-                                   [label color] (string/split s ":")]
-                               (set-property! :tags (concat (get-property :tags)
-                                                            [{:label label
-                                                              :value s
-                                                              :color color}])))))
-         :label-in-value true
-         :value          (or (get-property :tags) [])
-         :options        (->> (reduce
-                               (fn [result [day time-blocks]]
-                                 (reduce
-                                  (fn [result [time time-block]]
-                                    (if (nil? (:tags time-block))
-                                      result
-                                      (reduce (fn [result tag]
-                                                (conj result {:value (:value tag)}))
-                                              result
-                                              (:tags time-block))))
-                                  result
-                                  time-blocks))
-                               []
-                               (get-in @state [:timetable :time-blocks]))
-                              (distinct)
-                              (sort-by :value))
-         :tag-render     (fn [props]
-                           (let [{:strs [value closable onClose] :as props} (js->clj props)
-                                 [label color]                              (string/split value ":")]
-                             (r/as-element
-                              [tag/tag
-                               {:style         {:background-color color}
-                                :class         "tag"
-                                :on-mouse-down (fn [e]
-                                                 (.preventDefault e)
-                                                 (.stopPropagation e))
-                                :closable      closable
-                                :on-close      (fn [& args]
-                                                 (apply onClose args)
-                                                 (set-property! :tags (->> (get-property :tags)
-                                                                           (filter (comp (partial not= label) :label))
-                                                                           (vec))))}
-                               label])))}]]]
+      [tag-selector timeblocks (nil? selection) get-property set-property!]]
      [col/col {:lg 13 :sm 16}
       [:div.flex-fill {:style {:flex-grow 1}}
        [:div "Desc: "]
